f756b959033d1faf868fb65e4db38fe696aee25e
TresorNomenjanahary <jeantresor3@gmail.com>
2022-05-17 09:11:14 +0300

    ff

--- data.sql
+++ data.sql
@@ -16,17 +16,17 @@ CREATE TABLE Region(
 
 CREATE TABLE Article(
     idArticle SMALLINT PRIMARY KEY NOt NULL,
-    intitule VARCHAR(85) NOT NULL,
+    intitule VARCHAR(105) NOT NULL,
     description TEXT NOT NULL,
-    
+    date DATE,
+    image VARCHAR(20)
 );
 
 INSERT INTO Admin VALUES(1,'Jean','Batiste','jean@gmail.com',SHA1('12345678'));
 INSERT INTO Admin VALUES(2,'mika','Delanney','mika@gmail.com',SHA1('12345678'));
 
-INSERT INTO Region VALUES(1,'');
-INSERT INTO Region VALUES(2,'');
-INSERT INTO Region VALUES(3,'');
+INSERT INTO Region VALUES(1,'Ukraine');
+INSERT INTO Region VALUES(2,'France');
 
-INSERT INTO Article VALUES(3,'');
-INSERT INTO Article VALUES(3,'');
+INSERT INTO Article VALUES(1,'Guerre en Ukraine : agir pour la paix et le climat','Greenpeace condamne la guerre du gouvernement russe en Ukraine. Greenpeace condamne la guerre conduite par le gouvernement russe en Ukraine et appelle le Président Poutine à retirer immédiatement ses forces armées et à cesser les opérations militaires. Toutes nos pensées vont aux civils qui craignent actuellement pour leur vie ou sont obligés de fuir les combats. Ce conflit est une tragédie en premier lieu pour les Ukrainien·nes mais aussi pour la population russe, qui va être affectée par ses conséquences. L’urgence de passer à des énergies propres et sans danger. Au moment même où le GIEC sortait un nouveau rapport cataclysmique sur les conséquences du changement climatique éclatait la guerre en Ukraine, révélant tous les dangers des énergies qui font le socle de notre système énergétique. Gaz et pétrole financent la guerre du régime russe contre le peuple ukrainien tout comme ils alimentent des conflits partout dans le monde. Les énergies fossiles si nocives pour le climat le sont aussi pour la paix. L’industrie nucléaire française est quant à elle fortement dépendante de la Russie et pose de graves menaces pour la sécurité, qu’elle soit civile ou militaire. Il faut d’urgence en tirer les conséquences, briser cette addiction néfaste à des énergies mortifères et construire une transition énergétique alliant souveraineté, sécurité et défis climatiques.','2022-03-14','ukraine.jpg');
+INSERT INTO Article VALUES(2,'N’attendons pas 5 ans de plus pour le climat','Mobilisation pour la justice sociale et climatique, Comme lors de l’élection présidentielle, nous serons mobilisés pour ces élections législatives. Nous tenterons d’expliquer les enjeux environnementaux de ce scrutin. Nous le ferons avec le même engagement, de façon apartisane mais sans détour et sans concession, que ce soit pour défendre des propositions ambitieuses ou au contraire dénoncer des menaces graves pour la cause environnementale. D’ici là, d’autres temps forts nous attendent, à commencer par le grand rassemblement du 1er mai : nous étions dans la rue, pour la justice sociale et climatique, aux côtés d’autres organisations du mouvement climat et de nos partenaires de l’alliance pour une rupture écologique et sociale Plus jamais ça. Rejoignez-nous dans les semaines qui viennent via nos groupes locaux et nos réseaux en ligne !','2022-04-28','line.jpg');
--- index.php
+++ index.php
@@ -125,7 +125,7 @@
 
                     <div class="col-md-12 col-sm-12">
                          <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
-                              <h2>Meet our chefs</h2>
+                              <h2>Les actualités</h2>
                               <h4>They are nice &amp; friendly</h4>
                          </div>
                     </div>
@@ -136,10 +136,7 @@
                                    <div class="team-hover">
                                         <div class="team-item">
                                              <h4>Duis vel lacus id magna mattis vehicula</h4> 
-                                             <ul class="social-icon">
-                                                  <li><a href="#" class="fa fa-linkedin-square"></a></li>
-                                                  <li><a href="#" class="fa fa-envelope-o"></a></li>
-                                             </ul>
+                                         
                                         </div>
                                    </div>
                          </div>
@@ -155,10 +152,7 @@
                                    <div class="team-hover">
                                         <div class="team-item">
                                              <h4>Cras suscipit neque quis odio feugiat</h4>
-                                             <ul class="social-icon">
-                                                  <li><a href="#" class="fa fa-instagram"></a></li>
-                                                  <li><a href="#" class="fa fa-flickr"></a></li>
-                                             </ul>
+                                          
                                         </div>
                                    </div>
                          </div>
@@ -174,10 +168,7 @@
                                    <div class="team-hover">
                                         <div class="team-item">
                                              <h4>Etiam auctor enim tristique faucibus</h4>
-                                             <ul class="social-icon">
-                                                  <li><a href="#" class="fa fa-github"></a></li>
-                                                  <li><a href="#" class="fa fa-google"></a></li>
-                                             </ul>
+                                           >
                                         </div>
                                    </div>
                          </div>
@@ -190,141 +181,6 @@
                </div>
           </div>
      </section>
-
-
-     <!-- MENU-->
-     <section id="menu" data-stellar-background-ratio="0.5">
-          <div class="container">
-               <div class="row">
-
-                    <div class="col-md-12 col-sm-12">
-                         <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
-                              <h2>Our Menus</h2>
-                              <h4>Tea Time &amp; Dining</h4>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         <!-- MENU THUMB -->
-                         <div class="menu-thumb">
-                              <a href="images/menu-image1.jpg" class="image-popup" title="American Breakfast">
-                                   <img src="images/menu-image1.jpg" class="img-responsive" alt="">
-
-                                   <div class="menu-info">
-                                        <div class="menu-item">
-                                             <h3>American Breakfast</h3>
-                                             <p>Tomato / Eggs / Sausage</p>
-                                        </div>
-                                        <div class="menu-price">
-                                             <span>$25</span>
-                                        </div>
-                                   </div>
-                              </a>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         <!-- MENU THUMB -->
-                         <div class="menu-thumb">
-                              <a href="images/menu-image2.jpg" class="image-popup" title="Self-made Salad">
-                                   <img src="images/menu-image2.jpg" class="img-responsive" alt="">
-
-                                   <div class="menu-info">
-                                        <div class="menu-item">
-                                             <h3>Self-made Salad</h3>
-                                             <p>Green / Fruits / Healthy</p>
-                                        </div>
-                                        <div class="menu-price">
-                                             <span>$18</span>
-                                        </div>
-                                   </div>
-                              </a>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         <!-- MENU THUMB -->
-                         <div class="menu-thumb">
-                              <a href="images/menu-image3.jpg" class="image-popup" title="Chinese Noodle">
-                                   <img src="images/menu-image3.jpg" class="img-responsive" alt="">
-
-                                   <div class="menu-info">
-                                        <div class="menu-item">
-                                             <h3>Chinese Noodle</h3>
-                                             <p>Pepper / Chicken / Vegetables</p>
-                                        </div>
-                                        <div class="menu-price">
-                                             <span>$34</span>
-                                        </div>
-                                   </div>
-                              </a>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         <!-- MENU THUMB -->
-                         <div class="menu-thumb">
-                              <a href="images/menu-image4.jpg" class="image-popup" title="Rice Soup">
-                                   <img src="images/menu-image4.jpg" class="img-responsive" alt="">
-
-                                   <div class="menu-info">
-                                        <div class="menu-item">
-                                             <h3>Rice Soup</h3>
-                                             <p>Green / Chicken</p>
-                                        </div>
-                                        <div class="menu-price">
-                                             <span>$28</span>
-                                        </div>
-                                   </div>
-                              </a>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         <!-- MENU THUMB -->
-                         <div class="menu-thumb">
-                              <a href="images/menu-image5.jpg" class="image-popup" title="Project title">
-                                   <img src="images/menu-image5.jpg" class="img-responsive" alt="">
-
-                                   <div class="menu-info">
-                                        <div class="menu-item">
-                                             <h3>Deli Burger</h3>
-                                             <p>Beef / Fried Potatoes</p>
-                                        </div>
-                                        <div class="menu-price">
-                                             <span>$46</span>
-                                        </div>
-                                   </div>
-                              </a>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-6">
-                         <!-- MENU THUMB -->
-                         <div class="menu-thumb">
-                              <a href="images/menu-image6.jpg" class="image-popup" title="Project title">
-                                   <img src="images/menu-image6.jpg" class="img-responsive" alt="">
-
-                                   <div class="menu-info">
-                                        <div class="menu-item">
-                                             <h3>Big Flat Fried</h3>
-                                             <p>Pepper / Crispy</p>
-                                        </div>
-                                        <div class="menu-price">
-                                             <span>$30</span>
-                                        </div>
-                                   </div>
-                              </a>
-                         </div>
-                    </div>
-
-
-               </div>
-          </div>
-     </section>
-
-
-     <!-- TESTIMONIAL -->
      <section id="testimonial" data-stellar-background-ratio="0.5">
           <div class="overlay"></div>
           <div class="container">
@@ -339,29 +195,20 @@
                     <div class="col-md-offset-2 col-md-8 col-sm-12">
                          <div class="owl-carousel owl-theme">
                               <div class="item">
-                                   <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum ullamcorper nulla non.</p>
+                                   <p>Il faut sortir des énergies fossiles. Dans cette perspective, Greenpeace s’oppose à tout nouveau projet climaticide, qu’il s’agisse d’ouvrir une nouvelle centrale à charbon ou d'entreprendre un nouveau forage pétrolier.</p>
                                         <div class="tst-author">
-                                             <h4>Digital Carlson</h4>
-                                             <span>Pharetra quam sit amet</span>
+                                             <h4>Greenpeace</h4>
+                                             <span>Site internet </span>
                                         </div>
                               </div>
 
                               <div class="item">
-                                   <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vestibulum orci quam.</p>
+                                   <p>Sécheresse et montées des eaux entraînent des flux migratoires incontrôlés et des urgences humanitaires. Certaines îles du Pacifique commencent à disparaître, et les nations occidentales, jusqu’à présent épargnées, commencent à être exposées</p>
                                         <div class="tst-author">
-                                             <h4>Johnny Stephen</h4>
-                                             <span>Magna nisi porta ligula</span>
+                                             <h4>Greenpeace</h4>
+                                             <span>Site Internet</span>
                                         </div>
                               </div>
-
-                              <div class="item">
-                                   <p>Vivamus aliquet felis eu diam ultricies congue. Morbi porta lorem nec consectetur porta quis dui elit habitant morbi.</p>
-                                        <div class="tst-author">
-                                             <h4>Jessie White</h4>
-                                             <span>Vitae lacinia augue urna quis</span>
-                                        </div>
-                              </div>
-
                          </div>
                     </div>
 
