7f7b8d5a83fe5b451a8d8fe9fedbdd5b7da46485
TresorNomenjanahary <jeantresor3@gmail.com>
2022-05-17 09:23:50 +0300

    update

--- index.php
+++ index.php
@@ -67,8 +67,6 @@
           </div>
      </section>
 
-
-     <!-- HOME -->
      <section id="home" class="slider" data-stellar-background-ratio="0.5">
           <div class="row">
 
@@ -126,58 +124,42 @@
                     <div class="col-md-12 col-sm-12">
                          <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
                               <h2>Les actualités</h2>
-                              <h4>They are nice &amp; friendly</h4>
+                              <h4>Climat Planet est une organisation indépendante des Etats, des pouvoirs politiques et économiques.</h4>
                          </div>
                     </div>
-
+                    <?php for($i=0; $i<count($recentData); $i++){ ?>                                      
                     <div class="col-md-4 col-sm-4">
+                    
                          <div class="team-thumb wow fadeInUp" data-wow-delay="0.2s">
-                              <img src="images/team-image1.jpg" class="img-responsive" alt="">
+                              <img src=<?="images/".$recentData[$i]['image']?> class="img-responsive" alt="">
                                    <div class="team-hover">
                                         <div class="team-item">
-                                             <h4>Duis vel lacus id magna mattis vehicula</h4> 
-                                         
+                                             <h4><?=$recentData[$i]['intitule']?></h4> 
                                         </div>
                                    </div>
                          </div>
                          <div class="team-info">
-                              <h3>New Catherine</h3>
-                              <p>Kitchen Officer</p>
+                              <h3><?=$recentData[$i]['intitule']?></h3>
+                              <p><?=$recentData[$i]['date']?></p>
                          </div>
                     </div>
 
                     <div class="col-md-4 col-sm-4">
                          <div class="team-thumb wow fadeInUp" data-wow-delay="0.4s">
-                              <img src="images/team-image2.jpg" class="img-responsive" alt="">
+                              <img src=<?="images/".$recentData[$i]['image']?> class="img-responsive" alt="">
                                    <div class="team-hover">
                                         <div class="team-item">
-                                             <h4>Cras suscipit neque quis odio feugiat</h4>
+                                             <h4><?=$recentData[$i]['intitule']?></h4>
                                           
                                         </div>
                                    </div>
                          </div>
                          <div class="team-info">
-                              <h3>Lindsay Perlen</h3>
-                              <p>Owner &amp; Manager</p>
+                              <h3><?=$recentData[$i]['intitule']?></h3>
+                              <p><?=$recentData[$i]['date']?></p>
                          </div>
                     </div>
-
-                    <div class="col-md-4 col-sm-4">
-                         <div class="team-thumb wow fadeInUp" data-wow-delay="0.6s">
-                              <img src="images/team-image3.jpg" class="img-responsive" alt="">
-                                   <div class="team-hover">
-                                        <div class="team-item">
-                                             <h4>Etiam auctor enim tristique faucibus</h4>
-                                           >
-                                        </div>
-                                   </div>
-                         </div>
-                         <div class="team-info">
-                              <h3>Isabella Grace</h3>
-                              <p>Pizza Specialist</p>
-                         </div>
-                    </div>
-                    
+                    <?php }?>
                </div>
           </div>
      </section>
