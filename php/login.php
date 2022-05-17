f84c6968b16e3f5cb64eb25dd35992c34e728067
TresorNomenjanahary <jeantresor3@gmail.com>
2022-05-17 11:10:29 +0300

    ges

---
+++ php/login.php
@@ -0,0 +1,59 @@
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Climat Planet</title>
+
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+<body>
+    <div class="container-fluid ">
+        
+                <div class=" no-pdding login-box">
+                    <div class="row no-margin w-100 bklmj">
+                        <div class="col-lg-6 col-md-6 log-det">
+                            
+                            <h2>Login</h2>
+                            <div class="row no-margin past">
+                                <p>Connectez-vous, entrer vos identifiants. </p>
+                            </div>
+
+
+                            <div class="text-box-cont">
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Email" aria-label="email" aria-describedby="basic-addon1">
+                                </div>
+                                 <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Mot de passe" aria-label="mdp" aria-describedby="basic-addon1">
+                                </div>
+                                <div class="right-bkij mb-3">
+                                    <button class="btn btn-success btn-round">Connexion</button>
+                                </div> 
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 box-de">
+                            <div class="ditk-inf">
+                                <h2 class="w-100">Bienvenue Admin </h2>
+                                <p>Rejoigner l'équipe de Climat Planet <br> en vous connectant</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       
+</body>
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/script.js"></script>
+
+
+</html>
